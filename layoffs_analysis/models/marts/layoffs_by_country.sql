f8834da7e59fd9d2d3d4805f8db304c711086515
patricia-oliveira <patriciia.mota@gmail.com>
2022-12-15 17:08:36 +0000

    Refactor layoffs_by_country model using new macro

--- layoffs_analysis/models/marts/layoffs_by_country.sql
+++ layoffs_analysis/models/marts/layoffs_by_country.sql
@@ -4,14 +4,4 @@
     )
 }}
 
-{% set stg_layoffs = ref('stg_layoffs') %}
-
-SELECT
-    country,
-    SUM(total_laid_off) AS total_laid_off,
-    MIN(laid_off_at) AS layoff_started_at,
-    MAX(laid_off_at) AS layoff_updated_at
-FROM
-    {{ stg_layoffs }}
-GROUP BY
-    country
+{{ layoffs_aggregator('country') }}
