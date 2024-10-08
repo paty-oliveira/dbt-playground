0cc5d14842136445022bd38b2299bbc58dc84474
paty-oliveira <dev.patricia.oliveira@gmail.com>
2024-10-08 19:29:47 +0100

    Add stg_transcript model

---
+++ starbucks/macros/date_utils.sql
@@ -0,0 +1,3 @@
+{% macro get_days_from_hours(hour) %}
+    {{ hour }} / 24
+{% endmacro %}
---
+++ starbucks/models/staging/stg_transcript.sql
@@ -0,0 +1,8 @@
+SELECT
+    a AS transaction_id,
+    person AS customer_id,
+    event AS transaction_type,
+    value AS transaction_value,
+    time AS hours_since_start,
+    {{ get_days_from_hours('time') }} AS days_since_start
+FROM {{ ref('transcript') }}
