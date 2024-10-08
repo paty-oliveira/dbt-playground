81b4309bb09cb201c7bfea2cb5c67ae4e7666040
paty-oliveira <dev.patricia.oliveira@gmail.com>
2024-10-08 18:06:06 +0100

    Add documentation for seeds

--- starbucks/models/staging/stg_portfolio.sql
+++ starbucks/models/staging/stg_portfolio.sql
@@ -1,8 +1,8 @@
-SELECT 
+SELECT
     id AS portfolio_id,
     reward,
     duration AS duration_rank,
     difficulty AS difficulty_rank,
-    offer_type,
+    unnest(offer_type) as offer_type,
     channels
 FROM {{ ref('portfolio') }}
