19b15829c9e1698d967608fe5aee45d2c9a11233
paty-oliveira <dev.patricia.oliveira@gmail.com>
2024-10-11 19:36:49 +0100

    Add documentation about dim_date

--- starbucks/models/marts/dim_date.sql
+++ starbucks/models/marts/dim_date.sql
@@ -9,7 +9,8 @@ WITH date_spine AS (
 final AS (
     SELECT
         {{ dbt_utils.generate_surrogate_key(surrogate_key_columns) }} AS date_key,
-        *
+        *,
+        CURRENT_TIMESTAMP AS ingested_at
     FROM date_spine
 )
 
