4e54f8e2f75d0ac8d52a86a44a394d38db9b401b
paty-oliveira <dev.patricia.oliveira@gmail.com>
2024-10-11 19:09:26 +0100

    Add dim_date in marts schema

---
+++ starbucks/models/marts/dim_date.sql
@@ -0,0 +1,16 @@
+{% set surrogate_key_columns = ['year_number', 'month_of_year','day_of_month'] %}
+
+WITH date_spine AS (
+    {{
+        dbt_date.get_date_dimension("2000-01-01", "2100-12-31")
+    }}
+),
+
+final AS (
+    SELECT
+        {{ dbt_utils.generate_surrogate_key(surrogate_key_columns) }} AS date_key,
+        *
+    FROM date_spine
+)
+
+SELECT * FROM final
