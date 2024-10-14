9f3125b78685045e717aa23e3c7936d5f87228eb
paty-oliveira <dev.patricia.oliveira@gmail.com>
2024-10-14 19:16:56 +0100

    Add customer_profile_completed_offer model

--- starbucks/models/reports/customer_profile_received_offer.sql
+++
@@ -1,8 +0,0 @@
-{% set surrogate_columns = ['transaction_id', 'customer_id', 'offer_id', 'transaction_type'] %}
-
-{{ customer_profile_by_transaction_type(
-    'received',
-    surrogate_columns,
-    'customer_profile_received_offer_key'
-    )
-}}
---
+++ starbucks/models/reports/customers/customer_profile_completed_offer.sql
@@ -0,0 +1,9 @@
+{% set surrogate_columns = ['transaction_id', 'customer_id', 'offer_id', 'transaction_type'] %}
+
+{{
+    customer_profile_by_transaction_type(
+        'completed',
+        surrogate_columns,
+        'customer_profile_completed_offer_key'
+    )
+}}
